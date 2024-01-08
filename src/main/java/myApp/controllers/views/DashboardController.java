05b6f27899bf0b947dc2e96f47f4610ac0698d85
vinhnt2914 <113036284+vinhnt2914@users.noreply.github.com>
2024-01-08 22:37:49 +0700

    commit to save

--- src/main/java/myApp/controllers/views/DashboardController.java
+++ src/main/java/myApp/controllers/views/DashboardController.java
@@ -220,9 +220,9 @@ public class DashboardController implements Initializable {
 
         int userId = Main.getUserId();
 
-        String incomeQuery = "SELECT transactionDate, SUM(amount) AS total FROM transaction WHERE userID = ? AND category = 'Income' GROUP BY transactionDate ORDER BY transactionDate";
+        String incomeQuery = "SELECT transactionDate, SUM(amount) AS total FROM transaction WHERE userID = ? AND category IN ('Income', 'Dividend Income', 'Investment') GROUP BY transactionDate ORDER BY transactionDate";
        
-        String expensesQuery = "SELECT transactionDate, SUM(amount) AS total FROM transaction WHERE userID = ? AND category IN ('Rent', 'Subscription') GROUP BY transactionDate ORDER BY transactionDate";
+        String expensesQuery = "SELECT transactionDate, SUM(amount) AS total FROM transaction WHERE userID = ? AND category IN ('Rent', 'Subscription', 'Insurance', 'Bills') GROUP BY transactionDate ORDER BY transactionDate";
 
         updateTransactionMap(incomeQuery, userId, incomeMap, dbFormatter);
         updateTransactionMap(expensesQuery, userId, expensesMap, dbFormatter);
